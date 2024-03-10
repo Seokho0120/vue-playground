ec61b790dcc092dff0ba04185223c998e1b06675
Seokho <seokho0120@gmail.com>
2024-03-10 22:51:03 +0900

    fix: typescript 환경 설정, bootstrap 삭제

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.js';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
+import App from '@/App.vue';
 import router from './router';
 import dayjs from './plugins/dayjs';
 import './index.css';
