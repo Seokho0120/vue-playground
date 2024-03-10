4aad64526ada9dc0c171258d2823f311506f946d
Seokho <seokho0120@gmail.com>
2024-03-11 01:41:32 +0900

    add: primevue 적용

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { createPinia } from 'pinia';
 import App from '@/App.vue';
 import router from './router';
 import dayjs from './plugins/dayjs';
+import PrimeVue from 'primevue/config';
 import './index.css';
 
 const app = createApp(App);
+app.use(PrimeVue);
 app.use(router);
 app.use(dayjs);
 app.use(createPinia());
