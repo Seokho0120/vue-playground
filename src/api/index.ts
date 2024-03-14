5da2b5d527dbe87073fb11b3ee7cf886cecc0525
Seokho <seokho0120@gmail.com>
2024-03-14 23:35:13 +0900

    feat: axios api 모듈화 적용

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = `${import.meta.env.VITE_APP_API_URL}posts/`;
+const SERVER_URL = `${import.meta.env.VITE_APP_API_URL}posts`;
 
 const api = () => {
 	const instance = axios.create({
--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,26 +1,22 @@
 import type { FetchPostsResponse, Post, Form } from '@/types/posts';
-import axios from 'axios';
+import api from './index';
 
 export function getPosts(): Promise<FetchPostsResponse> {
-	return axios
-		.get<Post[]>('http://localhost:4000/posts')
-		.then(response => ({ data: response.data }));
-
-	// return axios.get<Post[]>('http://localhost:4000/posts');
+	return api().get<Post[]>('');
 }
 
 export function getPostById(id: string) {
-	return axios.get(`http://localhost:4000/posts/${id}`);
+	return api().get(`/${id}`);
 }
 
 export function createPosts(data: Post) {
-	return axios.post('http://localhost:4000/posts', data);
+	return api().post('', data);
 }
 
 export function updatePost(id: string, data: Form) {
-	return axios.patch(`http://localhost:4000/posts/${id}`, data);
+	return api().patch(`/${id}`, data);
 }
 
 export function deletePosts(id: string) {
-	return axios.delete(`http://localhost:4000/posts/${id}`);
+	return api().delete(`/${id}`);
 }
